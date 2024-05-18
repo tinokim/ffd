cd9d040abaa264402e6374057617bb15ebbcc07a
tinoKim <gksmfwlsgh@nate.com>
2024-05-18 15:54:17 +0900

    ++

---
+++ eurekaServer/src/test/java/com/ffd/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffd.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
